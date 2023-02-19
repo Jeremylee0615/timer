f7cdeade0db52973f8109cb9ff1d1c1b65db95d9
Jeremy Lee <jeremylee0615@gmail.com>
2023-02-18 22:31:01 -0800

    Simple alarm timer

---
+++ timer.js
@@ -0,0 +1,15 @@
+const input = process.argv.slice(2);
+let time = input.filter(time => {
+  return (time >= 0) && !isNaN(time);
+});
+const alarmTimer = function() {
+  for (const i of time) {
+    setTimeout(() => {
+      console.log('⌚⌚It is TIME!⌚⌚');
+      return process.stdout.write('\x07');
+    }, i * 1250);
+  }
+};
+
+alarmTimer(time);
+
